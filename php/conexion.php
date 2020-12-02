06a4b41367e8a7c765548cb171427b539d1ce4cc
jdanielh24 <jdanielopez24@gmail.com>
2020-12-01 20:55:56 -0600

    Crear archivo de conexion a bd

---
+++ php/conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $servidor="localhost";
+    $bd="carrito";
+    $usuario="root";
+    $pass="";
+    $conexion = new mysqli($servidor,$usuario,$pass,$bd);
+    if($conexion->connect_error){
+        die("Error. No se puede conectar a la base de datos.");
+    }
+?>
