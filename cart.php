d064a78a4d84379db8a2dceefe2969df2f8f356c
jdanielh24 <jdanielopez24@gmail.com>
2020-12-03 00:25:44 -0600

    checkout corregido

--- cart.php
+++ cart.php
@@ -91,7 +91,7 @@ if (isset($_SESSION['carrito'])) {
       <table class="table table-bordered fw-600 centrar-texto titulos">
         <thead>
           <tr class="titulo-tabla">
-            <th>Imagen</th>
+            <th class="imagen-tabla">Imagen</th>
             <th>Producto</th>
             <th>Precio</th>
             <th>Cantidad</th>
@@ -145,7 +145,7 @@ if (isset($_SESSION['carrito'])) {
         <div class="col-md-7">
           <div class="row">
             <div class="col-md-12 text-right border-bottom mb-5">
-              <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+              <h3 class="text-black h4 text-uppercase">Totales del carrito</h3>
             </div>
           </div>
           <div class="row mb-3">
@@ -167,7 +167,7 @@ if (isset($_SESSION['carrito'])) {
 
           <div class="row">
             <div class="col-md-12">
-              <button class="boton boton-amarillo btn btn-block" onclick="window.location='checkout.php'">Proceed To Checkout</button>
+              <button class="boton boton-amarillo btn btn-block" onclick="window.location='checkout.php'">Proceder a pagar</button>
             </div>
           </div>
         </div>
