e076173cb9120cf93e5596724cc1a6205bace43e
jdanielh24 <jdanielopez24@gmail.com>
2020-12-03 00:20:06 -0600

    Agregar checkout

--- cart.php
+++ cart.php
@@ -117,7 +117,7 @@ if (isset($_SESSION['carrito'])) {
                 <td>$<?php echo $arregloCarrito[$i]['Precio']; ?></td>
                 <td><?php echo $arregloCarrito[$i]['Cantidad']; ?></td>
                 <td>$<?php echo $arregloCarrito[$i]['Precio'] * $arregloCarrito[$i]['Cantidad']; ?></td>
-                <td><a href="#" class="btn btn-primary btn-sm btnEliminar" data-id="<?php echo $arregloCarrito[$i]['Id'];?>">X</a></td>
+                <td><a href="#" class="btn btn-primary btn-sm btnEliminar" data-id="<?php echo $arregloCarrito[$i]['Id']; ?>">X</a></td>
               </tr>
 
           <?php }
@@ -126,6 +126,53 @@ if (isset($_SESSION['carrito'])) {
         </tbody>
       </table>
     </form>
+  </div>
+
+  <div class="contenedor seccion ">
+    <div class="">
+      <div class="row mb-5">
+        <div class="col-md-6 mb-3 mb-md-0">
+          <button class="boton boton-amarillo btn btn-block">Actualizar Carrito</button>
+        </div>
+        <div class="col-md-6">
+          <button class="boton boton-amarillo btn btn-block">Continuar comprando</button>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-md-6 pl-5 seccion">
+      <div class="row justify-content-end">
+        <div class="col-md-7">
+          <div class="row">
+            <div class="col-md-12 text-right border-bottom mb-5">
+              <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <span class="text-black">Subtotal</span>
+            </div>
+            <div class="col-md-6 text-right">
+              <strong class="text-black">$230.00</strong>
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="col-md-6">
+              <span class="text-black">Total</span>
+            </div>
+            <div class="col-md-6 text-right">
+              <strong class="text-black">$230.00</strong>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12">
+              <button class="boton boton-amarillo btn btn-block" onclick="window.location='checkout.php'">Proceed To Checkout</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
   <?php include('layout/footer.php'); ?>
