a5a3207c9cfa55acc4beecc25d3e644061979226
jdanielh24 <jdanielopez24@gmail.com>
2020-12-05 02:17:23 -0600

    Restrigir admin a clientes

--- admin/productos.php
+++ admin/productos.php
@@ -1,12 +1,43 @@
-<?php 
-include('../php/conexion.php');
+<?php
 
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+  header('Location: index.php');
+}
+include '../php/conexion.php';
+
+/* crear una sentencia preparada */
+if ($stmt = $conexion->prepare("SELECT nivel FROM usuario WHERE id=?")) {
+
+  /* ligar parámetros para marcadores */
+  $stmt->bind_param("i", $_SESSION['user_id']);
+
+  /* execute query */
+  $stmt->execute();
+
+  /* bind result variables */
+  $stmt->bind_result($res);
+
+  /* fetch value */
+  $stmt->fetch();
+
+  $message = '';
+
+  if ($res != "admin") {
+    header("Location: ../index.php");
+  }
+
+  $stmt->close();
+}
 $resultado = $conexion->query("select * from productos order by id")or die($conexion->error);
+  /* cerrar sentencia */
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -34,234 +65,235 @@ $resultado = $conexion->query("select * from productos order by id")or die($cone
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
   <style>
-    .back-amarillo{
+    .back-amarillo {
       background-color: #ffd24f;
       border-color: #ffd24f;
     }
 
-    .btn2:hover{
-      background-color:  #e4bd48;
-      border-color:  #e4bd48;
+    .btn2:hover {
+      background-color: #e4bd48;
+      border-color: #e4bd48;
     }
   </style>
-<div class="wrapper">
+  <div class="wrapper">
 
-  <?php include "./layouts/header.php"; ?>
+    <?php include "./layouts/header.php"; ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Productos</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6 text-right">
-            <button type="button" class="btn2 back-amarillo btn btn-primary " data-toggle="modal" data-target="#exampleModal">
-            <i class="fa fa-plus"></i> Insertar Producto
-            </button>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0 text-dark">Productos</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6 text-right">
+              <button type="button" class="btn2 back-amarillo btn btn-primary " data-toggle="modal" data-target="#exampleModal">
+                <i class="fa fa-plus"></i> Insertar Producto
+              </button>
+              <a href="../logout.php" class="boton boton-amarillo fw-600">Cerrar sesión</a>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <?php 
-          if(isset($_GET['error'])){
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <?php
+          if (isset($_GET['error'])) {
 
-        ?>
-        <div class="alert alert-danger" role="alert">
-          <?php echo $_GET['error']; ?>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <?php } ?>
+          ?>
+            <div class="alert alert-danger" role="alert">
+              <?php echo $_GET['error']; ?>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          <?php } ?>
 
-        <?php 
-          if(isset($_GET['success'])){
+          <?php
+          if (isset($_GET['success'])) {
 
-        ?>
-        <div class="alert alert-success" role="alert">
-          Se ha insertado correctamente.
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <?php } ?>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nombre</th>
-              <th>Descripcion</th>
-              <th>Precio</th>
-              <th>Imagen</th>
-              <th>Inventario</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php 
-              while($f = mysqli_fetch_array($resultado)){
-
-              
-            ?>
+          ?>
+            <div class="alert alert-success" role="alert">
+              Se ha insertado correctamente.
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          <?php } ?>
+          <table class="table">
+            <thead>
               <tr>
-                <td><?php echo $f['id']; ?></td>
-                <td><?php echo $f['nombre']; ?></td>
-                <td><?php echo $f['descripcion']; ?></td>
-                <td><?php echo $f['precio']; ?></td>
-                <td><img src="../img/<?php echo $f['imagen'];?>" width="100px" height="100px"></td>
-                <td><?php echo $f['inventario']; ?></td>
-                <td>
-                  <button class="btn btn-danger btn-small btnEliminar" 
-                    data-id="<?php echo $f['id']; ?>" 
-                    data-toggle="modal" data-target="#modalEliminar">
-                    <i class="fa fa-trash"></i>
-                  </button>
-                </td>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Imagen</th>
+                <th>Inventario</th>
               </tr>
-            <?php 
+            </thead>
+            <tbody>
+              <?php
+              while ($f = mysqli_fetch_array($resultado)) {
+
+
+              ?>
+                <tr>
+                  <td><?php echo $f['id']; ?></td>
+                  <td><?php echo $f['nombre']; ?></td>
+                  <td><?php echo $f['descripcion']; ?></td>
+                  <td><?php echo $f['precio']; ?></td>
+                  <td><img src="../img/<?php echo $f['imagen']; ?>" width="100px" height="100px"></td>
+                  <td><?php echo $f['inventario']; ?></td>
+                  <td>
+                    <button class="btn btn-danger btn-small btnEliminar" data-id="<?php echo $f['id']; ?>" data-toggle="modal" data-target="#modalEliminar">
+                      <i class="fa fa-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+              <?php
               }
-            ?>
-          </tbody>
-        </table>
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  
-  <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <form action="../php/insertarProducto.php" method="POST" enctype="multipart/form-data">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Insertar Producto</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <div class="form-group">
-              <label for="nombre">Nombre</label>
-              <input type="text" name="nombre" placeolder="nombre" id="nombre" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label for="descripcion">Descripción</label>
-              <input type="text" name="descripcion" placeolder="descripcion" id="descripcion" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label for="precio">Precio</label>
-              <input type="number" min="0" name="precio" placeolder="precio" id="precio" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label for="imagen">Imagen</label>
-              <input type="file" name="imagen" id="imagen" class="form-control" required>
-          </div>
-          <div class="form-group">
-              <label for="inventario">Inventario</label>
-              <input type="number" min="0" name="inventario" placeolder="inventario" id="inventario" class="form-control" required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn2 back-amarillo btn btn-primary ">Guardar</button>
-        </div>
-      </form>
+              ?>
+            </tbody>
+          </table>
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
     </div>
-  </div>
-</div>
 
-  <!-- Modal Eliminar-->
-  <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      
-        <div class="modal-header">
-          <h5 class="modal-title" id="modalEliminarLabel">Eliminar Producto</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <form action="../php/insertarProducto.php" method="POST" enctype="multipart/form-data">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Insertar Producto</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" placeolder="nombre" id="nombre" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="descripcion">Descripción</label>
+                <input type="text" name="descripcion" placeolder="descripcion" id="descripcion" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="precio">Precio</label>
+                <input type="number" min="0" name="precio" placeolder="precio" id="precio" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="imagen">Imagen</label>
+                <input type="file" name="imagen" id="imagen" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="inventario">Inventario</label>
+                <input type="number" min="0" name="inventario" placeolder="inventario" id="inventario" class="form-control" required>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn2 back-amarillo btn btn-primary ">Guardar</button>
+            </div>
+          </form>
         </div>
-        <div class="modal-body">
-          ¿Desea eliminar el producto?
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-danger eliminar" data-dismiss="modal">Eliminar</button>
-        </div>
-      
+      </div>
     </div>
+
+    <!-- Modal Eliminar-->
+    <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalEliminarLabel">Eliminar Producto</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ¿Desea eliminar el producto?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-danger eliminar" data-dismiss="modal">Eliminar</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <?php include "./layouts/footer.php"; ?>
+
   </div>
-</div>
+  <!-- ./wrapper -->
 
-  <?php include "./layouts/footer.php"; ?>
+  <!-- jQuery -->
+  <script src="./dashboard/plugins/jquery/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="./dashboard/plugins/jquery-ui/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="./dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- ChartJS -->
+  <script src="./dashboard/plugins/chart.js/Chart.min.js"></script>
+  <!-- Sparkline -->
+  <script src="./dashboard/plugins/sparklines/sparkline.js"></script>
+  <!-- JQVMap -->
+  <script src="./dashboard/plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="./dashboard/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="./dashboard/plugins/jquery-knob/jquery.knob.min.js"></script>
+  <!-- daterangepicker -->
+  <script src="./dashboard/plugins/moment/moment.min.js"></script>
+  <script src="./dashboard/plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- Tempusdominus Bootstrap 4 -->
+  <script src="./dashboard/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <!-- Summernote -->
+  <script src="./dashboard/plugins/summernote/summernote-bs4.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="./dashboard/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="./dashboard/dist/js/adminlte.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="./dashboard/dist/js/pages/dashboard.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="./dashboard/dist/js/demo.js"></script>
 
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="./dashboard/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="./dashboard/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="./dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="./dashboard/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="./dashboard/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="./dashboard/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="./dashboard/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="./dashboard/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="./dashboard/plugins/moment/moment.min.js"></script>
-<script src="./dashboard/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="./dashboard/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="./dashboard/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="./dashboard/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="./dashboard/dist/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="./dashboard/dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="./dashboard/dist/js/demo.js"></script>
-
-<script>
-  $(document).ready(function(){
-    var idEliminar = -1;
-    var fila;
-    $(".btnEliminar").click(function(){
-      idEliminar = $(this).data('id');
-      fila = $(this).parent('td').parent('tr');
-    });
-    $(".eliminar").click(function(){
-      $.ajax({
-        url: '../php/eliminarProducto.php',
-        method: 'POST',
-        data:{
-          id: idEliminar
-        }
-      }).done(function(res){
-        $(fila).fadeOut(1000);
+  <script>
+    $(document).ready(function() {
+      var idEliminar = -1;
+      var fila;
+      $(".btnEliminar").click(function() {
+        idEliminar = $(this).data('id');
+        fila = $(this).parent('td').parent('tr');
+      });
+      $(".eliminar").click(function() {
+        $.ajax({
+          url: '../php/eliminarProducto.php',
+          method: 'POST',
+          data: {
+            id: idEliminar
+          }
+        }).done(function(res) {
+          $(fila).fadeOut(1000);
+        });
       });
     });
-  });
-</script>
+  </script>
 </body>
+
 </html>
