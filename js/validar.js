b91cb8dbd83bc4b3aee49e61a564d7b7468ac839
jdanielh24 <jdanielopez24@gmail.com>
2020-12-30 16:46:06 -0600

    Agregar validacion del correo

--- js/validar.js
+++ js/validar.js
@@ -5,8 +5,19 @@ function validar(){
     passw = document.getElementById("c_password").value;
     passw2 = document.getElementById("c_password2").value;
 
+    expresion = /\w+@\.+[a-z]/;
+
     if(nombre==="" || email==="" || passw==="" || passw2===""){
         alert("Todos los campos son obligatorios.");
+        return false;
+    }else if(nombre.length > 200){
+        alert("El nombre es muy largo");
+        return false;
+    }else if(email.length > 200){
+        alert("El correo es muy largo");
+        return false;
+    }else if(!expresion.test(email)){
+        alert("El email no es valido");
         return false;
     }
 
